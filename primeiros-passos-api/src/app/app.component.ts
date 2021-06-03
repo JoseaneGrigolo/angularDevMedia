61ef11c44bf0b7301484f236fa534acf9e7239f2
JoseaneGrigolo <joseaneig@gmail.com>
2021-06-03 14:10:43 -0300

    api testes

---
+++ primeiros-passos-api/src/app/app.component.ts
@@ -0,0 +1,89 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { Produto } from '../models/produto.model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  readonly apiURL!: string;
+  constructor(private http: HttpClient) {
+    this.apiURL = 'http://localhost:3000';
+  }
+
+
+  //REQUISIÇÃO GET
+  listarTodosProdutos() {
+    this.http.get<Produto[]>(`${this.apiURL}/produtos`)
+      .subscribe((resultado: any) => console.log(resultado));
+  }
+  //REQUISIÇÃO GET por id
+  listarProdutoPorId() {
+    this.http.get<Produto>(`${this.apiURL}/produtos/1`)
+      .subscribe((resultado: any) => console.log(resultado),
+        erro => {
+          if (erro.status == 404) {
+            console.log('Produto não localizado.');
+          }
+        }
+      );
+  }
+  //REQUISIÇÃO POST
+  adicionarProduto() {
+    var produto =  new Produto();
+    produto.nome = "Cadeira Gamer";
+
+    this.http.post<Produto>(`${this.apiURL}/produtos`, produto)
+      .subscribe(
+        (resultado: any) => {
+          console.log(resultado)
+        },
+        erro => {
+          if (erro.status == 400) {
+            console.log(erro.error.mensagem);
+          }
+        }
+      );
+  }
+
+  //REQUISIÇÃO PUT
+  alterarProduto() {
+    var produto = new Produto();
+    produto.nome = "Smart TV";
+
+    this.http.put(`${this.apiURL}/produtos/1`, produto)
+      .subscribe(
+        (resultado: any) => {
+          console.log('Produto alterado com sucesso.')
+        },
+        erro => {
+          switch (erro.status) {
+            case 400:
+              console.log(erro.error.mensagem);
+              break;
+            case 404:
+              console.log('Produto não localizado.');
+              break;
+          }
+        }
+      );
+  }
+  //REQUISIÇÃO DELETE
+  excluirProduto() {
+    this.http.delete(`${this.apiURL}/produtos/1`)
+      .subscribe(
+        (resultado: any) => {
+          console.log('Produto excluído com sucesso.');
+        },
+        erro => {
+          if (erro.status == 404) {
+            console.log('Produto não localizado.');
+          }
+        }
+      );
+  }
+
+}
